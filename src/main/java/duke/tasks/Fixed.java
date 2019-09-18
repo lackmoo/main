b44006aa93ebb39d7ddfe1150be6054aa8cb7270
Andrew Lau <andrew@Andrews-MacBook-Pro-2.local>
2019-09-18 21:08:55 +0800

    Add JavaDoc comment for Fixed.java

--- src/main/java/duke/tasks/Fixed.java
+++ src/main/java/duke/tasks/Fixed.java
@@ -3,11 +3,22 @@ package duke.tasks;
 public class Fixed extends Task {
     private String fixedDuration;
 
+    /**
+     * Constructor for Duke.Tasks.Fixed object.
+     * @param taskName name or description of the fixed duration task.
+     * @param fixedDuration duration of the fixed duration task.
+     */
     public Fixed(String taskName, String fixedDuration) {
         super(taskName);
         this.fixedDuration = fixedDuration;
     }
 
+    /**
+     * This constructor is used for recreation of Duke.Tasks.Fixed from storage.
+     * @param done 1 if task has been marked complete, 0 otherwise.
+     * @param taskName the name or description of the fixed duration task.
+     * @param fixedDuration duration of the fixed duration task.
+     */
     public Fixed(int done, String taskName, String fixedDuration) {
         super(taskName);
         this.isDone = (done == 1);
