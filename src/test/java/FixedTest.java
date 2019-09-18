1cf5749d79dad518b04baf49c5295a4963a1f8c8
Andrew Lau <andrew@Andrews-MacBook-Pro-2.local>
2019-09-18 21:42:26 +0800

    Update FixedTest with correct expected output

--- src/test/java/FixedTest.java
+++ src/test/java/FixedTest.java
@@ -9,20 +9,20 @@ class FixedTest {
     @org.junit.jupiter.api.Test
     void testToString() {
         Task t = new Fixed("read sales report", "2 hours");
-        assertEquals("F|0|read sales report|2 hours", t.toString());
-        assertNotEquals("F|1|read sales report|2 hours", t.toString());
+        assertEquals("[F][✗] read sales report (needs: 2 hours)", t.toString());
+        assertNotEquals("[F][✓] read sales report (needs: 2 hours)", toString());
         t.markDone();
-        assertEquals("F|1|read sales report|2 hours", t.toString());
-        assertNotEquals("F|0|read sales report|2 hours", t.toString());
+        assertEquals("[F][✓] read sales report (needs: 2 hours)", t.toString());
+        assertNotEquals("[F][✗] read sales report (needs: 2 hours)", toString());
     }
 
     @org.junit.jupiter.api.Test
     void storeString() {
         Task t = new Fixed("read sales report", "2 hours");
-        assertEquals("[F][✗] read sales report (needs: 2 hours)", t.storeString());
-        assertNotEquals("[F][✓] read sales report (needs: 2 hours)", t.storeString());
+        assertEquals("F | 0 | read sales report | 2 hours", t.storeString());
+        assertNotEquals("F | 1 | read sales report | 2 hours", t.storeString());
         t.markDone();
-        assertEquals("[F][✓] read sales report (needs: 2 hours)", t.storeString());
-        assertNotEquals("[F][✗] read sales report (needs: 2 hours)", t.storeString());
+        assertEquals("F | 1 | read sales report | 2 hours", t.storeString());
+        assertNotEquals("F | 0 | read sales report | 2 hours", t.storeString());
     }
 }
