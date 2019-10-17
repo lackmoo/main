ad3929b8d03af0ba5e8e2cc5c2fd9b367c3720a9
Andrew Lau <andrew@Andrews-MacBook-Pro-6.local>
2019-10-18 01:20:46 +0800

    Implement Remove-multiple command and fix bugs

--- src/main/java/spinbox/commands/MultipleCommand.java
+++ src/main/java/spinbox/commands/MultipleCommand.java
@@ -1,5 +1,10 @@
 package spinbox.commands;
 
+import spinbox.containers.ModuleContainer;
+import spinbox.containers.lists.FileList;
+import spinbox.entities.Notepad;
+import spinbox.entities.items.File;
+import spinbox.entities.Module;
 import spinbox.exceptions.SpinBoxException;
 import spinbox.exceptions.InputException;
 import spinbox.Storage;
@@ -7,85 +12,119 @@ import spinbox.entities.items.tasks.Task;
 import spinbox.Ui;
 import spinbox.containers.lists.TaskList;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class MultipleCommand extends Command {
-    private String commandType;
-    private String indexes;
+    private static final String NON_EXISTENT_MODULE = "This module does not exist.";
+    private static final String NOTES_REMOVED = "The specified notes have been successfully removed from ";
+    private static final String PROVIDE_INDEX = "Please provide an index to be removed.";
+    private String type;
+
+    private String moduleCode;
+    private String content;
 
     /**
-     * Constructor for multiple command.
-     * @param commandType Either done-multiple or delete-multiple.
-     * @param indexes Indexes of the tasks to be deleted.
+     * Constructor for initialization of variables to support removal of entities.
+     * @param moduleCode A String denoting the module code.
+     * @param content A string containing the content of the processed user input.
      */
-    public MultipleCommand(String commandType, String indexes) {
-        this.commandType = commandType;
-        this.indexes = indexes;
+    public MultipleCommand(String moduleCode, String content) {
+        this.moduleCode = moduleCode;
+        this.content = content;
+        this.type = content.split(" ")[0];
     }
 
-    /**
-     * Either delete multiple relevant tasks from the task list or
-     * mark them as done and delete them.
-     * @param taskList TaskList instance.
-     * @param storage Storage instance.
-     * @param ui Ui instance.
-     * @throws SpinBoxException Invalid index or storage error.
-     */
     @Override
-    public String execute(TaskList taskList, Storage storage, Ui ui) throws SpinBoxException {
-        int inputSize = indexes.split(" ").length;
-        if (inputSize > 1) {
+    public String execute(ModuleContainer moduleContainer, ArrayDeque<String> pageTrace, Ui ui) throws
+            SpinBoxException {
+        int inputSize = content.split(" ").length;
+        if (inputSize == 1) {
+            throw new InputException(PROVIDE_INDEX);
+        }
+        if (inputSize > 2) {
             throw new InputException("Ensure that the indexes are separated by ',' without any spacing. "
-                    + "E.g. delete-multiple 2,3,4 or done-multiple 2,3,4");
+                    + "E.g. remove-multiple <pageContent> : <type> 2,3,4");
         }
         try {
-            String[] splitIndexes = indexes.split(",");
+            String[] splitIndexes = content.replace(type.concat(" "), "").split(",");
             if (splitIndexes.length == 1) {
-                throw new InputException("To delete or mark a single task as done, provide the input in this "
-                        + "format instead: delete <one index in integer form> or done <one index in integer form>.");
+                throw new InputException("To remove a single task, provide the input in this "
+                        + "format instead: remove <pageContent> : <type> <one index in integer form>.");
             }
-            List<String> formattedOutput = new ArrayList<>();
             List<Integer> finalIndexes = new ArrayList<>();
-            for (int i = 0; i < splitIndexes.length; i++) {
-                String convert = splitIndexes[i];
+            for (String convert : splitIndexes) {
                 finalIndexes.add(Integer.parseInt(convert) - 1);
             }
-            Collections.sort(finalIndexes, Collections.reverseOrder());
-            if (commandType.equals("delete-multiple")) {
-                for (int i = 0; i < finalIndexes.size(); i++) {
-                    Task removed = taskList.remove(finalIndexes.get(i));
-                    if (i == 0) {
-                        formattedOutput.add("Noted. I've removed these tasks:\n" + removed.toString());
-                    } else {
-                        formattedOutput.add(removed.toString());
+            finalIndexes.sort(Collections.reverseOrder());
+            StringBuilder outputMessage = new StringBuilder();
+            switch (type) {
+            case "file":
+                if (moduleContainer.checkModuleExists(moduleCode)) {
+                    HashMap<String, Module> modules = moduleContainer.getModules();
+                    Module module = modules.get(moduleCode);
+                    FileList files = module.getFiles();
+                    for (int i = 0; i < finalIndexes.size(); i++) {
+                        File fileRemoved = files.remove(finalIndexes.get(i));
+                        if (i == 0) {
+                            outputMessage.append("Noted. I've removed these tasks:\n");
+                            outputMessage.append(fileRemoved.toString()).append("\n");
+                        } else {
+                            outputMessage.append(fileRemoved.toString()).append("\n");
+                        }
                     }
+                    outputMessage.append("You currently have ").append(files.getList().size()).append((
+                            files.getList().size() == 1) ? " file in the list." : " files in the list.");
+                    return outputMessage.toString();
+                } else {
+                    return NON_EXISTENT_MODULE;
                 }
-            } else {
-                for (int i = 0; i < finalIndexes.size(); i++) {
-                    Task completed = taskList.mark(finalIndexes.get(i));
-                    taskList.remove(finalIndexes.get(i));
-                    if (i == 0) {
-                        formattedOutput.add("Nice! I've marked these tasks as done:\n" + completed.toString());
-                    } else {
-                        formattedOutput.add(completed.toString());
+
+            case "note":
+                if (moduleContainer.checkModuleExists(moduleCode)) {
+                    HashMap<String, Module> modules = moduleContainer.getModules();
+                    Module module = modules.get(moduleCode);
+                    Notepad notepad = module.getNotepad();
+                    for (Integer finalIndex : finalIndexes) {
+                        notepad.removeLine(finalIndex);
                     }
-                    if (i == finalIndexes.size() - 1) {
-                        formattedOutput.add("The above tasks have also been removed from the list of tasks.");
-                    }
+                    return NOTES_REMOVED + moduleCode;
+                } else {
+                    return NON_EXISTENT_MODULE;
                 }
+
+            case "task":
+                if (moduleContainer.checkModuleExists(moduleCode)) {
+                    HashMap<String, Module> modules = moduleContainer.getModules();
+                    Module module = modules.get(moduleCode);
+                    TaskList tasks = module.getTasks();
+                    for (int i = 0; i < finalIndexes.size(); i++) {
+                        Task taskRemoved = tasks.remove(finalIndexes.get(i));
+                        if (i == 0) {
+                            outputMessage.append("Noted. I've removed these tasks:\n");
+                            outputMessage.append(taskRemoved.toString()).append("\n");
+                        } else {
+                            outputMessage.append(taskRemoved.toString()).append("\n");
+                        }
+                    }
+                    outputMessage.append("You currently have ").append(tasks.getList().size()).append((
+                            tasks.getList().size() == 1) ? " task in the list." : " tasks in the list.");
+                    return outputMessage.toString();
+                } else {
+                    return NON_EXISTENT_MODULE;
+                }
+
+            default:
+                throw new InputException("Please use valid remove format:\n"
+                        + "remove <pageContent> : <type> <index>");
             }
-            List<Task> tasks = taskList.getList();
-            taskList.saveData();
-            formattedOutput.add("You currently have " + tasks.size()
-                    + ((tasks.size() == 1) ? " task in the list." : " tasks in the list."));
-            return ui.showFormatted(formattedOutput);
         } catch (IndexOutOfBoundsException e) {
-            throw new InputException("Invalid index entered. Type 'list' to see your list.");
+            throw new InputException("Invalid index entered.");
         } catch (NumberFormatException e) {
-            throw new InputException("Invalid index entered. Please ensure the index is in integer form. "
-                    + "E.g. delete-multiple 2,5 or done-multiple 2,5");
+            throw new InputException("Invalid index entered. Please ensure the index is in integer form.");
         }
     }
 }
--- src/main/java/spinbox/commands/RemoveCommand.java
+++ src/main/java/spinbox/commands/RemoveCommand.java
@@ -2,9 +2,11 @@ package spinbox.commands;
 
 import spinbox.containers.ModuleContainer;
 import spinbox.containers.lists.FileList;
+import spinbox.containers.lists.TaskList;
 import spinbox.entities.Notepad;
 import spinbox.entities.items.File;
 import spinbox.entities.Module;
+import spinbox.entities.items.tasks.Task;
 import spinbox.exceptions.SpinBoxException;
 import spinbox.exceptions.InputException;
 import spinbox.Ui;
@@ -15,6 +17,7 @@ import java.util.HashMap;
 public class RemoveCommand extends Command {
     private static final String NON_EXISTENT_MODULE = "This module does not exist.";
     private static final String NOTE_REMOVED = "A note has been successfully removed from ";
+    private static final String PROVIDE_INDEX = "Please provide an index to be removed.";
     private String type;
 
     private String moduleCode;
@@ -34,7 +37,9 @@ public class RemoveCommand extends Command {
     @Override
     public String execute(ModuleContainer moduleContainer, ArrayDeque<String> pageTrace, Ui ui) throws
             SpinBoxException {
-
+        if (content.split(" ").length == 1) {
+            throw new InputException(PROVIDE_INDEX);
+        }
         switch (type) {
         case "file":
             if (moduleContainer.checkModuleExists(moduleCode)) {
@@ -45,7 +50,9 @@ public class RemoveCommand extends Command {
                     int index = Integer.parseInt(content.split(" ")[1]) - 1;
                     File fileRemoved = files.get(index);
                     files.remove(index);
-                    return "Removed file: " + fileRemoved.toString();
+                    return "Removed file: " + fileRemoved.toString() + "\n"
+                            + "You currently have " + files.getList().size()
+                            + ((files.getList().size() == 1) ? " file in the list." : " files in the list.");
                 } catch (NumberFormatException e) {
                     throw new InputException("Please enter a valid index.");
                 }
@@ -61,6 +68,25 @@ public class RemoveCommand extends Command {
                 int index = Integer.parseInt(content.split(" ")[1]) - 1;
                 notepad.removeLine(index);
                 return NOTE_REMOVED + moduleCode;
+            } else {
+                return NON_EXISTENT_MODULE;
+            }
+
+        case "task":
+            if (moduleContainer.checkModuleExists(moduleCode)) {
+                try {
+                    HashMap<String, Module> modules = moduleContainer.getModules();
+                    Module module = modules.get(moduleCode);
+                    TaskList tasks = module.getTasks();
+                    int index = Integer.parseInt(content.split(" ")[1]) - 1;
+                    Task taskRemoved = tasks.get(index);
+                    tasks.remove(index);
+                    return "Removed task: " + taskRemoved.toString() + "\n"
+                            + "You currently have " + tasks.getList().size()
+                            + ((tasks.getList().size() == 1) ? " task in the list." : " tasks in the list.");
+                } catch (NumberFormatException e) {
+                    throw new InputException("Please enter a valid index.");
+                }
             } else {
                 return NON_EXISTENT_MODULE;
             }
