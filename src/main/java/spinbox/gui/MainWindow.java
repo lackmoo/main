fa450b910d66264d1241eb789c89ba4bab0e4d41
Andrew Lau <andrew@Andrews-MacBook-Pro-8.local>
2019-11-07 13:04:14 +0800

    Add index to task display

--- src/main/java/spinbox/gui/MainWindow.java
+++ src/main/java/spinbox/gui/MainWindow.java
@@ -420,8 +420,9 @@ public class MainWindow extends GridPane {
 
         TaskList taskList = currModule.getTasks();
         List<Task> tasks = taskList.getList();
-        for (Task task : tasks) {
-            String description = task.getTaskType().name();
+        for (int i = 0; i < tasks.size(); i += 1) {
+            Task task = tasks.get(i);
+            String description = (i + 1) + ". " + task.getTaskType().name();
             description += ": " + task.getName();
             String dates = "";
             if (task.isSchedulable()) {
