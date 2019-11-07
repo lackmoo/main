8e9df4ff36ee6e02bbd6749692d691be7b2c6b72
Andrew Lau <andrew@Andrews-MacBook-Pro-8.local>
2019-11-07 17:26:32 +0800

    Add Integration Tests for Set-Date and Set-Name

--- src/test/java/integration/TaskCommandsIntegrationTest.java
+++ src/test/java/integration/TaskCommandsIntegrationTest.java
@@ -2,6 +2,10 @@ package integration;
 
 import org.junit.jupiter.api.Test;
 import spinbox.DateTime;
+import spinbox.Parser;
+import spinbox.Ui;
+import spinbox.commands.Command;
+import spinbox.containers.ModuleContainer;
 import spinbox.containers.lists.TaskList;
 import spinbox.entities.Module;
 import spinbox.entities.items.tasks.Deadline;
@@ -12,17 +16,19 @@ import spinbox.entities.items.tasks.Lecture;
 import spinbox.entities.items.tasks.Task;
 import spinbox.entities.items.tasks.Todo;
 import spinbox.entities.items.tasks.Tutorial;
-import spinbox.exceptions.CorruptedDataException;
-import spinbox.exceptions.DataReadWriteException;
-import spinbox.exceptions.FileCreationException;
+
 import spinbox.exceptions.InvalidIndexException;
+import spinbox.exceptions.StorageException;
+import spinbox.exceptions.SpinBoxException;
+
+import java.util.ArrayDeque;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskCommandsIntegrationTest {
     @Test
     public void loadDataSuccessful_AddTasksThenManualClear_successfulRepopulationOfData() throws
-            DataReadWriteException, FileCreationException, CorruptedDataException, InvalidIndexException {
+            StorageException, InvalidIndexException {
         Module testModuleOne = new Module("testMod6", "Engineering Principles & Practice IV");
 
         TaskList taskList = testModuleOne.getTasks();
@@ -65,5 +71,77 @@ public class TaskCommandsIntegrationTest {
         assertEquals(taskList.get(5).toString(), "[TUT][NOT DONE] Test 7 (at: 01/06/2019 10:00"
                 + " to 01/06/2019 12:00)");
         assertEquals(taskList.get(6).toString(), "[T][NOT DONE] Test 1");
+    }
+
+    @Test
+    public void setNameSuccessful_setNameOfTaskToANewName_taskNameSuccessfullySet() throws
+            SpinBoxException {
+        ModuleContainer testContainer = new ModuleContainer();
+        Module testModule = new Module("CG1112", "Engineering Principles & Practice III");
+        testContainer.addModule(testModule);
+
+        TaskList taskList = testModule.getTasks();
+        while (taskList.getList().size() > 0) {
+            taskList.getList().remove(0);
+        }
+        Task testTask1 = new Todo("Test 1");
+        Task testTask2 = new Lab("Test 2", new DateTime("01/04/2019 10:00"),
+                new DateTime("01/04/2019 12:00"));
+
+        taskList.add(testTask1);
+        taskList.add(testTask2);
+
+        ArrayDeque<String> pageTrace = new ArrayDeque<>();
+        Ui ui = new Ui(true);
+
+        String setNameForTask1 = "set-name CG1112 / task 1 to: Lab";
+        Parser.setPageTrace(pageTrace);
+        Command command = Parser.parse(setNameForTask1);
+        command.execute(testContainer, pageTrace, ui, false);
+
+        String setNameForTask2 = "set-name CG1112 / task 2 to: Todo";
+        Parser.setPageTrace(pageTrace);
+        command = Parser.parse(setNameForTask2);
+        command.execute(testContainer, pageTrace, ui, false);
+
+        assertEquals(taskList.get(0).toString(), "[LAB][NOT DONE] Lab (at: 01/04/2019 10:00"
+                + " to 01/04/2019 12:00)");
+        assertEquals(taskList.get(1).toString(), "[T][NOT DONE] Todo");
+    }
+
+    @Test
+    public void setDateSuccessful_setDateOfSchedulableTaskToANewDate_taskDateSuccessfullySet() throws
+            SpinBoxException {
+        ModuleContainer testContainer = new ModuleContainer();
+        Module testModule = new Module("CG1112", "Engineering Principles & Practice III");
+        testContainer.addModule(testModule);
+
+        TaskList taskList = testModule.getTasks();
+        while (taskList.getList().size() > 0) {
+            taskList.getList().remove(0);
+        }
+        Task testTask1 = new Deadline("Test 1", new DateTime("01/01/2019 10:00"));
+        Task testTask2 = new Exam("Test 2", new DateTime("12/12/2019 10:00"),
+                new DateTime("01/03/2019 12:00"));
+
+        taskList.add(testTask1);
+        taskList.add(testTask2);
+
+        ArrayDeque<String> pageTrace = new ArrayDeque<>();
+        Ui ui = new Ui(true);
+
+        String setDateForTask1 = "set-date CG1112 / task 1 to: 02/01/2019 12:00";
+        Parser.setPageTrace(pageTrace);
+        Command command = Parser.parse(setDateForTask1);
+        command.execute(testContainer, pageTrace, ui, false);
+
+        String setDateForTask2 = "set-date CG1112 / task 2 to: 02/05/2019 16:00 to 02/05/2019 19:00";
+        Parser.setPageTrace(pageTrace);
+        command = Parser.parse(setDateForTask2);
+        command.execute(testContainer, pageTrace, ui, false);
+
+        assertEquals(taskList.get(0).toString(), "[D][NOT DONE] Test 1 (by: 02/01/2019 12:00)");
+        assertEquals(taskList.get(1).toString(), "[EXAM][NOT DONE] Test 2 (at: 02/05/2019 16:00"
+                + " to 02/05/2019 19:00)");
     }
 }
